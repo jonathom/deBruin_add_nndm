77f431ee83d72613be1ad049cceb9a31006d74e7
jonathom <jonathanbahlmann@web.de>
2022-08-26 16:31:46 +0200

    get install script

---
+++ R/install.R
@@ -0,0 +1,10 @@
+# future.apply  globals  listenv  NNDM  parallelly
+# R.version # 4
+.libPaths("/home/j/j_bahl03/R")
+
+# install.packages("CAST", version="0.5.1")
+install.packages("emdist", repos='http://cran.us.r-project.org')
+# remove.packages("globals")
+# install.packages("globals", repos='http://cran.us.r-project.org')
+
+
